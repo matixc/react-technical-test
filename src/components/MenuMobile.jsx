1097a8b6ec273f8686470d819c6d716b8690735a
Mateusz Dziezok <mateusz.dziezok@gmail.com>
2019-05-18 12:42:05 +0200

    Getting started
    cleaning
    installs
    MobileMenu with placeholders

---
+++ src/components/MenuMobile.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+
+const MobileMenu = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 100%;
+    background-color: #fd6365;
+    color: white;
+`;
+
+// TODO temp - to be removed
+const Placeholder = styled.div`
+    text-align: center;
+    height: 25vh;
+`;
+
+function MenuMobile() {
+    return (
+        <MobileMenu>
+            <Placeholder>header placeholder</Placeholder>
+            <Placeholder>user info placeholder</Placeholder>
+            <Placeholder>menu list placeholder</Placeholder>
+            <Placeholder>footer placeholder</Placeholder>
+        </MobileMenu>
+    );
+}
+
+export default MenuMobile;
